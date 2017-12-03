5c2f43a634031034592971ae314d8e1fcfd2e641
Mert <m.mertgn@gmail.com>
2017-12-03 19:00:51 +0300

    Veritabanı Güncellendi.

--- Hastane.PL/FormAnaSayfa.Designer.cs
+++ Hastane.PL/FormAnaSayfa.Designer.cs
@@ -578,6 +578,7 @@
             this.Controls.Add(this.pnlHeader);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormAnaSayfa";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
--- Hastane.PL/FormGiris.Designer.cs
+++ Hastane.PL/FormGiris.Designer.cs
@@ -30,15 +30,15 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormGiris));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btnCikis = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.btnGiris = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtKullaniciAdi = new ns1.BunifuMetroTextbox();
-            this.txtSifre = new ns1.BunifuMetroTextbox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.txtSifre = new ns1.BunifuMetroTextbox();
+            this.txtKullaniciAdi = new ns1.BunifuMetroTextbox();
+            this.btnCikis = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -66,6 +66,62 @@
             this.panel1.Size = new System.Drawing.Size(360, 400);
             this.panel1.TabIndex = 0;
             // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Hastane.PL.Properties.Resources.hospital_2;
+            this.pictureBox3.Location = new System.Drawing.Point(43, 258);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(49, 43);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 19;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Hastane.PL.Properties.Resources.hospital_1;
+            this.pictureBox2.Location = new System.Drawing.Point(43, 209);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(49, 43);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.BorderColorFocused = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(52)))));
+            this.txtSifre.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtSifre.BorderColorMouseHover = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(52)))));
+            this.txtSifre.BorderThickness = 3;
+            this.txtSifre.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtSifre.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txtSifre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(52)))));
+            this.txtSifre.isPassword = true;
+            this.txtSifre.Location = new System.Drawing.Point(90, 258);
+            this.txtSifre.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(229, 44);
+            this.txtSifre.TabIndex = 17;
+            this.txtSifre.Text = "Şifre";
+            this.txtSifre.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.BorderColorFocused = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(52)))));
+            this.txtKullaniciAdi.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtKullaniciAdi.BorderColorMouseHover = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(52)))));
+            this.txtKullaniciAdi.BorderThickness = 3;
+            this.txtKullaniciAdi.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtKullaniciAdi.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txtKullaniciAdi.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(52)))));
+            this.txtKullaniciAdi.isPassword = false;
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(90, 208);
+            this.txtKullaniciAdi.Margin = new System.Windows.Forms.Padding(4);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(229, 44);
+            this.txtKullaniciAdi.TabIndex = 16;
+            this.txtKullaniciAdi.Text = "Kullanıcı Adı";
+            this.txtKullaniciAdi.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
             // btnCikis
             // 
             this.btnCikis.BackColor = System.Drawing.Color.Firebrick;
@@ -89,6 +145,16 @@
             this.label2.Size = new System.Drawing.Size(327, 37);
             this.label2.TabIndex = 5;
             this.label2.Text = "Hastane Otomasyonu";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(122, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(119, 105);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
             // 
             // btnGiris
             // 
@@ -114,72 +180,6 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Kullanıcı Girişi";
             // 
-            // txtKullaniciAdi
-            // 
-            this.txtKullaniciAdi.BorderColorFocused = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(52)))));
-            this.txtKullaniciAdi.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.txtKullaniciAdi.BorderColorMouseHover = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(52)))));
-            this.txtKullaniciAdi.BorderThickness = 3;
-            this.txtKullaniciAdi.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.txtKullaniciAdi.Font = new System.Drawing.Font("Century Gothic", 9.75F);
-            this.txtKullaniciAdi.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(52)))));
-            this.txtKullaniciAdi.isPassword = false;
-            this.txtKullaniciAdi.Location = new System.Drawing.Point(90, 208);
-            this.txtKullaniciAdi.Margin = new System.Windows.Forms.Padding(4);
-            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
-            this.txtKullaniciAdi.Size = new System.Drawing.Size(229, 44);
-            this.txtKullaniciAdi.TabIndex = 16;
-            this.txtKullaniciAdi.Text = "Kullanıcı Adı";
-            this.txtKullaniciAdi.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // txtSifre
-            // 
-            this.txtSifre.BorderColorFocused = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(52)))));
-            this.txtSifre.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.txtSifre.BorderColorMouseHover = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(52)))));
-            this.txtSifre.BorderThickness = 3;
-            this.txtSifre.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.txtSifre.Font = new System.Drawing.Font("Century Gothic", 9.75F);
-            this.txtSifre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(43)))), ((int)(((byte)(52)))));
-            this.txtSifre.isPassword = true;
-            this.txtSifre.Location = new System.Drawing.Point(90, 258);
-            this.txtSifre.Margin = new System.Windows.Forms.Padding(4);
-            this.txtSifre.Name = "txtSifre";
-            this.txtSifre.Size = new System.Drawing.Size(229, 44);
-            this.txtSifre.TabIndex = 17;
-            this.txtSifre.Text = "Şifre";
-            this.txtSifre.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = global::Hastane.PL.Properties.Resources.hospital_2;
-            this.pictureBox3.Location = new System.Drawing.Point(43, 258);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(49, 43);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 19;
-            this.pictureBox3.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::Hastane.PL.Properties.Resources.hospital_1;
-            this.pictureBox2.Location = new System.Drawing.Point(43, 209);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(49, 43);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 18;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(122, 19);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(119, 105);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
-            // 
             // FormGiris
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -189,6 +189,7 @@
             this.Controls.Add(this.panel1);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormGiris";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
