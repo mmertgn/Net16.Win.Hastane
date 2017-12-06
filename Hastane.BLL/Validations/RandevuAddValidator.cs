430dc9b9fdb7a73425fc8face33fdc7d3fa5cc76
Mustafa Mert Gün <m.mertgn@gmail.com>
2017-12-06 16:08:37 +0300

    Update RandevuAddValidator.cs

--- Hastane.BLL/Validations/RandevuAddValidator.cs
+++ Hastane.BLL/Validations/RandevuAddValidator.cs
@@ -13,6 +13,7 @@ namespace Hastane.BLL.Validations
         public RandevuAddValidator()
         {
             RuleFor(x => x.HastaID).NotEmpty().WithMessage("Hastayı sol taraftaki listeden seçmek zorundasınız!");
+            //BurasıFarklı
             //asdfghjkl
             RuleFor(x => x.Saat).NotEmpty().WithMessage("Bir Randevu Saati Seçmek Zorundasınız!");
         }
